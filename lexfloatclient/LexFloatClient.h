1e2bff5fdbea9fc26b99c13984ca9a36befbbf02
Adnan Kamili <adnan.kamili@gmail.com>
2021-03-25 11:32:05 +0530

    updated c src header files

---
+++ lexfloatclient/LexFloatClient.h
@@ -0,0 +1,266 @@
+/* LexFloatClient.h */
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+#include "LexFloatStatusCodes.h"
+
+#ifdef _WIN32
+    /*
+    Make sure you're using the MSVC or Intel compilers on Windows.
+    */
+    #include <windows.h>
+
+	#ifdef LEXFLOATCLIENT_EXPORTS
+        #ifdef LEXFLOATCLIENT_STATIC
+            #define LEXFLOATCLIENT_API extern "C"
+        #else
+            #define LEXFLOATCLIENT_API extern "C" __declspec(dllexport)
+        #endif
+	#else
+		#ifdef __cplusplus
+            #ifdef LEXFLOATCLIENT_STATIC
+                #define LEXFLOATCLIENT_API extern "C"
+            #else
+                #define LEXFLOATCLIENT_API extern "C" __declspec(dllimport)
+            #endif
+        #else
+            #ifdef LEXFLOATCLIENT_STATIC
+                #define LEXFLOATCLIENT_API
+            #else
+                #define LEXFLOATCLIENT_API __declspec(dllimport)
+            #endif
+        #endif
+	#endif
+
+    #if defined(USE_STDCALL_DLL) && !defined(LEXFLOATCLIENT_STATIC)
+        #define LF_CC __stdcall
+    #else
+        #define LF_CC __cdecl
+    #endif
+    typedef const wchar_t* CSTRTYPE;
+	typedef wchar_t* STRTYPE;
+#else
+    #define LF_CC
+    #if __GNUC__ >= 4
+        #ifdef __cplusplus
+            #define LEXFLOATCLIENT_API extern "C" __attribute__((visibility("default")))
+        #else
+            #define LEXFLOATCLIENT_API __attribute__((visibility("default")))
+        #endif
+    #else
+        #ifdef __cplusplus
+            #define LEXFLOATCLIENT_API extern "C"
+        #else
+            #define LEXFLOATCLIENT_API
+        #endif
+    #endif
+	typedef const char* CSTRTYPE;
+	typedef char* STRTYPE;
+#endif
+typedef void (LF_CC *CallbackType)(uint32_t);
+
+/*
+    FUNCTION: SetHostProductId()
+
+    PURPOSE: Sets the product id of your application.
+
+    PARAMETERS:
+    * productId - the unique product id of your application as mentioned
+      on the product page in the dashboard.
+
+    RETURN CODES: LF_OK, LF_E_PRODUCT_ID
+*/
+LEXFLOATCLIENT_API int LF_CC SetHostProductId(CSTRTYPE productId);
+
+/*
+    FUNCTION: SetHostUrl()
+
+    PURPOSE: Sets the network address of the LexFloatServer.
+
+    The url format should be: http://[ip or hostname]:[port]
+
+    PARAMETERS:
+    * hostUrl - url string having the correct format
+
+    RETURN CODES: LF_OK, LF_E_PRODUCT_ID, LF_E_HOST_URL
+*/
+LEXFLOATCLIENT_API int LF_CC SetHostUrl(CSTRTYPE hostUrl);
+
+/*
+    FUNCTION: SetFloatingLicenseCallback()
+
+    PURPOSE: Sets the renew license callback function.
+
+    Whenever the license lease is about to expire, a renew request is sent to the
+    server. When the request completes, the license callback function
+    gets invoked with one of the following status codes:
+
+    LF_OK, LF_E_INET, LF_E_LICENSE_EXPIRED_INET, LF_E_LICENSE_NOT_FOUND, LF_E_CLIENT, LF_E_IP,
+    LF_E_SERVER, LF_E_TIME, LF_E_SERVER_LICENSE_NOT_ACTIVATED,LF_E_SERVER_TIME_MODIFIED,
+    LF_E_SERVER_LICENSE_SUSPENDED, LF_E_SERVER_LICENSE_EXPIRED, LF_E_SERVER_LICENSE_GRACE_PERIOD_OVER
+
+    PARAMETERS:
+    * callback - name of the callback function
+
+    RETURN CODES: LF_OK, LF_E_PRODUCT_ID
+*/
+LEXFLOATCLIENT_API int LF_CC SetFloatingLicenseCallback(CallbackType callback);
+
+/*
+    FUNCTION: SetFloatingClientMetadata()
+
+    PURPOSE: Sets the floating client metadata.
+
+    The  metadata appears along with the license details of the license
+    in LexFloatServer dashboard.
+
+    PARAMETERS:
+    * key - string of maximum length 256 characters with utf-8 encoding.
+    * value - string of maximum length 256 characters with utf-8 encoding.
+
+    RETURN CODES: LF_OK, LF_E_PRODUCT_ID, LF_E_METADATA_KEY_LENGTH,
+    LF_E_METADATA_VALUE_LENGTH, LF_E_ACTIVATION_METADATA_LIMIT
+*/
+LEXFLOATCLIENT_API int LF_CC SetFloatingClientMetadata(CSTRTYPE key, CSTRTYPE value);
+
+/*
+    FUNCTION: GetHostLicenseMetadata()
+
+    PURPOSE: Get the value of the license metadata field associated with the LexFloatServer license.
+
+    PARAMETERS:
+    * key - key of the metadata field whose value you want to get
+    * value - pointer to a buffer that receives the value of the string
+    * length - size of the buffer pointed to by the value parameter
+
+    RETURN CODES: LF_OK, LF_E_PRODUCT_ID, LF_E_NO_LICENSE, LF_E_BUFFER_SIZE,
+    LF_E_METADATA_KEY_NOT_FOUND
+*/
+LEXFLOATCLIENT_API int LF_CC GetHostLicenseMetadata(CSTRTYPE key, STRTYPE value, uint32_t length);
+
+/*
+    FUNCTION: GetHostLicenseMeterAttribute()
+
+    PURPOSE: Gets the license meter attribute allowed uses and total uses associated with the LexFloatServer license.
+
+    PARAMETERS:
+    * name - name of the meter attribute
+    * allowedUses - pointer to the integer that receives the value
+    * totalUses - pointer to the integer that receives the value
+    * grossUses - pointer to the integer that receives the value
+
+    RETURN CODES: LF_OK, LF_E_PRODUCT_ID, LF_E_NO_LICENSE, LF_E_METER_ATTRIBUTE_NOT_FOUND
+*/
+LEXFLOATCLIENT_API int LF_CC GetHostLicenseMeterAttribute(CSTRTYPE name, uint32_t *allowedUses, uint32_t *totalUses, uint32_t *grossUses);
+
+/*
+    FUNCTION: GetHostLicenseExpiryDate()
+
+    PURPOSE: Gets the license expiry date timestamp of the LexFloatServer license.
+
+    PARAMETERS:
+    * expiryDate - pointer to the integer that receives the value
+
+    RETURN CODES: LF_OK, LF_E_PRODUCT_ID, LF_E_NO_LICENSE
+*/
+LEXFLOATCLIENT_API int LF_CC GetHostLicenseExpiryDate(uint32_t *expiryDate);
+
+/*
+    FUNCTION: GetFloatingClientMeterAttributeUses()
+
+    PURPOSE: Gets the meter attribute uses consumed by the floating client.
+
+    PARAMETERS:
+    * name - name of the meter attribute
+    * allowedUses - pointer to the integer that receives the value
+
+    RETURN CODES: LF_OK, LF_E_PRODUCT_ID, LF_E_NO_LICENSE, LF_E_METER_ATTRIBUTE_NOT_FOUND
+*/
+LEXFLOATCLIENT_API int LF_CC GetFloatingClientMeterAttributeUses(CSTRTYPE name, uint32_t *uses);
+
+/*
+    FUNCTION: RequestFloatingLicense()
+
+    PURPOSE: Sends the request to lease the license from the LexFloatServer.
+
+    RETURN CODES: LF_OK, LF_FAIL, LF_E_PRODUCT_ID, LF_E_LICENSE_EXISTS, LF_E_HOST_URL,
+    LF_E_CALLBACK, LF_E_LICENSE_LIMIT_REACHED, LF_E_INET, LF_E_TIME, LF_E_CLIENT, LF_E_IP, LF_E_SERVER,
+    LF_E_SERVER_LICENSE_NOT_ACTIVATED, LF_E_SERVER_TIME_MODIFIED, LF_E_SERVER_LICENSE_SUSPENDED,
+    LF_E_SERVER_LICENSE_GRACE_PERIOD_OVER, LF_E_SERVER_LICENSE_EXPIRED
+*/
+LEXFLOATCLIENT_API int LF_CC RequestFloatingLicense();
+
+/*
+    FUNCTION: DropFloatingLicense()
+
+    PURPOSE: Sends the request to the LexFloatServer to free the license.
+
+    Call this function before you exit your application to prevent zombie licenses.
+
+    RETURN CODES: LF_OK, LF_E_PRODUCT_ID, LF_E_NO_LICENSE, LF_E_HOST_URL, LF_E_CALLBACK,
+    LF_E_INET, LF_E_LICENSE_NOT_FOUND, LF_E_CLIENT, LF_E_IP, LF_E_SERVER,
+    LF_E_SERVER_LICENSE_NOT_ACTIVATED, LF_E_SERVER_TIME_MODIFIED, LF_E_SERVER_LICENSE_SUSPENDED,
+    LF_E_SERVER_LICENSE_GRACE_PERIOD_OVER, LF_E_SERVER_LICENSE_EXPIRED
+*/
+LEXFLOATCLIENT_API int LF_CC DropFloatingLicense();
+
+/*
+    FUNCTION: HasFloatingLicense()
+
+    PURPOSE: Checks whether any license has been leased or not. If yes,
+    it retuns LF_OK.
+
+    RETURN CODES: LF_OK, LF_E_PRODUCT_ID, LF_E_NO_LICENSE
+*/
+LEXFLOATCLIENT_API int LF_CC HasFloatingLicense();
+
+/*
+    FUNCTION: IncrementFloatingClientMeterAttributeUses()
+
+    PURPOSE: Increments the meter attribute uses of the floating client.
+
+    PARAMETERS:
+    * name - name of the meter attribute
+    * increment - the increment value
+
+    RETURN CODES: LF_OK, LF_E_PRODUCT_ID, LF_E_NO_LICENSE, LF_E_HOST_URL, LF_E_METER_ATTRIBUTE_NOT_FOUND,
+    LF_E_INET, LF_E_LICENSE_NOT_FOUND, LF_E_CLIENT, LF_E_IP, LF_E_SERVER, LF_E_METER_ATTRIBUTE_USES_LIMIT_REACHED,
+    LF_E_SERVER_LICENSE_NOT_ACTIVATED, LF_E_SERVER_TIME_MODIFIED, LF_E_SERVER_LICENSE_SUSPENDED,
+    LF_E_SERVER_LICENSE_GRACE_PERIOD_OVER, LF_E_SERVER_LICENSE_EXPIRED
+
+*/
+LEXFLOATCLIENT_API int LF_CC IncrementFloatingClientMeterAttributeUses(CSTRTYPE name, uint32_t increment);
+
+/*
+    FUNCTION: DecrementFloatingClientMeterAttributeUses()
+
+    PURPOSE: Decrements the meter attribute uses of the floating client.
+
+    PARAMETERS:
+    * name - name of the meter attribute
+    * decrement - the decrement value
+
+    RETURN CODES: LF_OK, LF_E_PRODUCT_ID, LF_E_NO_LICENSE, LF_E_HOST_URL, LF_E_METER_ATTRIBUTE_NOT_FOUND,
+    LF_E_INET, LF_E_LICENSE_NOT_FOUND, LF_E_CLIENT, LF_E_IP, LF_E_SERVER,
+    LF_E_SERVER_LICENSE_NOT_ACTIVATED, LF_E_SERVER_TIME_MODIFIED, LF_E_SERVER_LICENSE_SUSPENDED,
+    LF_E_SERVER_LICENSE_GRACE_PERIOD_OVER, LF_E_SERVER_LICENSE_EXPIRED
+
+    NOTE: If the decrement is more than the current uses, it resets the uses to 0.
+*/
+LEXFLOATCLIENT_API int LF_CC DecrementFloatingClientMeterAttributeUses(CSTRTYPE name, uint32_t decrement);
+
+/*
+    FUNCTION: ResetFloatingClientMeterAttributeUses()
+
+    PURPOSE: Resets the meter attribute uses consumed by the floating client.
+
+    PARAMETERS:
+    * name - name of the meter attribute
+
+    RETURN CODES: LF_OK, LF_E_PRODUCT_ID, LF_E_NO_LICENSE, LF_E_HOST_URL, LF_E_METER_ATTRIBUTE_NOT_FOUND,
+    LF_E_INET, LF_E_LICENSE_NOT_FOUND, LF_E_CLIENT, LF_E_IP, LF_E_SERVER,
+    LF_E_SERVER_LICENSE_NOT_ACTIVATED, LF_E_SERVER_TIME_MODIFIED, LF_E_SERVER_LICENSE_SUSPENDED,
+    LF_E_SERVER_LICENSE_GRACE_PERIOD_OVER, LF_E_SERVER_LICENSE_EXPIRED
+*/
+LEXFLOATCLIENT_API int LF_CC ResetFloatingClientMeterAttributeUses(CSTRTYPE name);
---
+++ lexfloatclient/LexFloatStatusCodes.h
@@ -0,0 +1,200 @@
+#ifndef LEX_FLOAT_STATUS_CODES_H
+#define LEX_FLOAT_STATUS_CODES_H
+
+enum LexFloatStatusCodes
+{
+    /*
+        CODE: LF_OK
+
+        MESSAGE: Success code.
+    */
+    LF_OK = 0,
+
+    /*
+        CODE: LF_FAIL
+
+        MESSAGE: Failure code.
+    */
+    LF_FAIL = 1,
+
+    /*
+        CODE: LF_E_PRODUCT_ID
+
+        MESSAGE: The product id is incorrect.
+    */
+    LF_E_PRODUCT_ID = 40,
+
+    /*
+        CODE: LF_E_CALLBACK
+
+        MESSAGE: Invalid or missing callback function.
+    */
+    LF_E_CALLBACK = 41,
+
+    /*
+        CODE: LF_E_HOST_URL
+
+        MESSAGE: Missing or invalid server url.
+    */
+    LF_E_HOST_URL = 42,
+
+    /*
+        CODE: LF_E_TIME
+
+        MESSAGE: Ensure system date and time settings are correct.
+    */
+    LF_E_TIME = 43,
+
+    /*
+        CODE: LF_E_INET
+
+        MESSAGE: Failed to connect to the server due to network error.
+    */
+    LF_E_INET = 44,
+
+    /*
+        CODE: LF_E_NO_LICENSE
+
+        MESSAGE: License has not been leased yet.
+    */
+    LF_E_NO_LICENSE = 45,
+
+    /*
+        CODE: LF_E_LICENSE_EXISTS
+
+        MESSAGE: License has already been leased.
+    */
+    LF_E_LICENSE_EXISTS = 46,
+
+    /*
+        CODE: LF_E_LICENSE_NOT_FOUND
+
+        MESSAGE: License does not exist on server or has already expired. This
+        happens when the request to refresh the license is delayed.
+    */
+    LF_E_LICENSE_NOT_FOUND = 47,
+
+    /*
+        CODE: LF_E_LICENSE_EXPIRED_INET
+
+        MESSAGE: License lease has expired due to network error. This
+        happens when the request to refresh the license fails due to
+        network error.
+    */
+    LF_E_LICENSE_EXPIRED_INET = 48,
+
+    /*
+        CODE: LF_E_LICENSE_LIMIT_REACHED
+
+        MESSAGE: The server has reached it's allowed limit of floating licenses.
+    */
+    LF_E_LICENSE_LIMIT_REACHED = 49,
+
+    /*
+        CODE: LF_E_BUFFER_SIZE
+
+        MESSAGE: The buffer size was smaller than required.
+    */
+    LF_E_BUFFER_SIZE = 50,
+
+    /*
+        CODE: LF_E_METADATA_KEY_NOT_FOUND
+
+        MESSAGE: The metadata key does not exist.
+    */
+    LF_E_METADATA_KEY_NOT_FOUND = 51,
+
+    /*
+        CODE: LF_E_METADATA_KEY_LENGTH
+
+        MESSAGE: Metadata key length is more than 256 characters.
+    */
+    LF_E_METADATA_KEY_LENGTH = 52,
+
+    /*
+        CODE: LF_E_METADATA_VALUE_LENGTH
+
+        MESSAGE: Metadata value length is more than 256 characters.
+    */
+    LF_E_METADATA_VALUE_LENGTH = 53,
+
+    /*
+        CODE: LF_E_ACTIVATION_METADATA_LIMIT
+
+        MESSAGE: The floating client has reached it's metadata fields limit.
+    */
+    LF_E_FLOATING_CLIENT_METADATA_LIMIT = 54,
+
+    /*
+        CODE: LF_E_METER_ATTRIBUTE_NOT_FOUND
+
+        MESSAGE: The meter attribute does not exist.
+    */
+    LF_E_METER_ATTRIBUTE_NOT_FOUND = 55,
+
+    /*
+        CODE: LF_E_METER_ATTRIBUTE_USES_LIMIT_REACHED
+
+        MESSAGE: The meter attribute has reached it's usage limit.
+    */
+    LF_E_METER_ATTRIBUTE_USES_LIMIT_REACHED = 56,
+
+    /*
+        CODE: LF_E_IP
+
+        MESSAGE: IP address is not allowed.
+    */
+    LF_E_IP = 60,
+
+    /*
+        CODE: LF_E_CLIENT
+
+        MESSAGE: Client error.
+    */
+    LF_E_CLIENT = 70,
+
+    /*
+        CODE: LF_E_SERVER
+
+        MESSAGE: Server error.
+    */
+    LF_E_SERVER = 71,
+
+    /*
+        CODE: LF_E_SERVER_TIME_MODIFIED
+
+        MESSAGE: System time on server has been tampered with. Ensure
+        your date and time settings are correct on the server machine.
+    */
+    LF_E_SERVER_TIME_MODIFIED = 72,
+
+    /*
+        CODE: LF_E_SERVER_LICENSE_NOT_ACTIVATED
+
+        MESSAGE: The server has not been activated using a license key.
+    */
+    LF_E_SERVER_LICENSE_NOT_ACTIVATED = 73,
+
+    /*
+        CODE: LF_E_SERVER_LICENSE_EXPIRED
+
+        MESSAGE: The server license has expired.
+    */
+    LF_E_SERVER_LICENSE_EXPIRED = 74,
+
+    /*
+        CODE: LF_E_SERVER_LICENSE_SUSPENDED
+
+        MESSAGE: The server license has been suspended.
+    */
+    LF_E_SERVER_LICENSE_SUSPENDED = 75,
+
+    /*
+        CODE: LF_E_SERVER_LICENSE_GRACE_PERIOD_OVER
+
+        MESSAGE: The grace period for server license is over.
+    */
+    LF_E_SERVER_LICENSE_GRACE_PERIOD_OVER = 76
+};
+
+#endif // LEX_FLOAT_STATUS_CODES_H
