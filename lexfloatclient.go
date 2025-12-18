b54df9f679c39cfec164725fc4d243247b686b7f
Huzaif Mushtaq Mir <huzaif@Huzaifs-MacBook-Air.local>
2025-12-18 14:05:03 +0530

    feat: add expiresAt to featureEntitlements

--- lexfloatclient.go
+++ lexfloatclient.go
@@ -27,6 +27,7 @@ type HostFeatureEntitlement struct {
 	FeatureName          string `json:"featureName"`
 	FeatureDisplayName   string `json:"featureDisplayName"`
 	Value                string `json:"value"`
+	ExpiresAt            uint   `json:"expiresAt"`
 }
 
 type callbackType func(int)
